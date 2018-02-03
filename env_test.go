46935cfdde08d7e0d359acddd6be3d4b59d06a1d
Songmu <y.songmu@gmail.com>
2018-02-03 20:20:16 +0900

    env_test.go

--- env_test.go
+++ env_test.go
@@ -2,6 +2,69 @@ package checron
 
 import "testing"
 
+func TestNewEnv(t *testing.T) {
+	testCases := []struct {
+		Name  string
+		Input string
+
+		Valid bool
+		Key   string
+		Val   string
+	}{
+		{
+			Name:  "normal",
+			Input: "hoge=fuga",
+
+			Valid: true,
+			Key:   "hoge",
+			Val:   "fuga",
+		},
+		{
+			Name:  "trim space",
+			Input: " hoge = fuga ",
+
+			Valid: true,
+			Key:   "hoge",
+			Val:   "fuga",
+		},
+		{
+			Name:  "quoted and needs trimming",
+			Input: ` "hoge" = 'fuga' `,
+
+			Valid: true,
+			Key:   "hoge",
+			Val:   "fuga",
+		},
+		{
+			Name:  "invalid",
+			Input: `'ho'ge'='fuga'`,
+
+			Valid: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			env := newEnv(tc.Input)
+			if !tc.Valid {
+				if env.Err() == nil {
+					t.Errorf("error should be occurred but nil: %#v", env)
+				}
+				return
+			}
+			if env.Err() != nil {
+				t.Errorf("error should be nil but: %s", env.Err())
+			}
+			if tc.Key != env.Key() {
+				t.Errorf("invalid key. out=%q, expect=%q", env.Key(), tc.Key)
+			}
+			if tc.Val != env.Val() {
+				t.Errorf("invalid val. out=%q, expect=%q", env.Val(), tc.Val)
+			}
+		})
+	}
+}
+
 func TestDequote(t *testing.T) {
 	testCases := []struct {
 		Name    string

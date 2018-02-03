e1b060a2771a93b5db1e0e36f91f07f27148199d
Songmu <y.songmu@gmail.com>
2018-02-03 16:13:46 +0900

    crontab.Err

--- crontab.go
+++ crontab.go
@@ -2,6 +2,7 @@ package checron
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"regexp"
 	"strings"
@@ -25,8 +26,11 @@ const (
 )
 
 type Crontab struct {
+	raw     string
 	entries []Entry
 	env     map[string]string
+
+	errors errors
 }
 
 func Parse(rdr io.Reader, hasUser bool) (*Crontab, error) {
@@ -38,25 +42,8 @@ func Parse(rdr io.Reader, hasUser bool) (*Crontab, error) {
 	return ct, scr.Err()
 }
 
-var jobReg = regexp.MustCompile(`^\s*(?:@|\*|[0-9])`)
-
-func (ct *Crontab) parseLine(line string, hasUser bool) Entry {
-	switch {
-	case strings.HasPrefix(line, "#"):
-		return &Comment{raw: line}
-	case strings.TrimSpace(line) == "":
-		return &Empty{raw: line}
-	case jobReg.MatchString(line):
-		return newJob(line, hasUser, cloneMap(ct.env))
-	case strings.Contains(line, "="):
-		env := newEnv(line)
-		if env.Err() == nil {
-			ct.env[env.Key()] = env.Val()
-		}
-		return env
-	default:
-		return &Invalid{raw: line}
-	}
+func (ct *Crontab) Raw() string {
+	return ct.raw
 }
 
 func (ct *Crontab) Entries() []Entry {
@@ -72,13 +59,37 @@ func (ct *Crontab) Jobs() (jobs []*Job) {
 	return jobs
 }
 
-func (ct *Crontab) Valid() bool {
-	for _, ent := range ct.entries {
-		if ent.Err() != nil {
-			return false
+func (ct *Crontab) Err() error {
+	if ct.errors == nil {
+		ct.errors = []error{}
+		for i, ent := range ct.entries {
+			if ent.Err() != nil {
+				ct.errors = append(ct.errors, fmt.Errorf("line %d: %s", i+1, ent.Err().Error()))
+			}
 		}
 	}
-	return true
+	return ct.errors.err()
+}
+
+var jobReg = regexp.MustCompile(`^\s*(?:@|\*|[0-9])`)
+
+func (ct *Crontab) parseLine(line string, hasUser bool) Entry {
+	switch {
+	case strings.HasPrefix(strings.TrimSpace(line), "#"):
+		return &Comment{raw: line}
+	case strings.TrimSpace(line) == "":
+		return &Empty{raw: line}
+	case jobReg.MatchString(line):
+		return newJob(line, hasUser, cloneMap(ct.env))
+	case strings.Contains(line, "="):
+		env := newEnv(line)
+		if env.Err() == nil {
+			ct.env[env.Key()] = env.Val()
+		}
+		return env
+	default:
+		return &Invalid{raw: line}
+	}
 }
 
 func cloneMap(orig map[string]string) map[string]string {
